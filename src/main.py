8c41c15baf25f28679b1d69c0353da7b547cee34
MYZ <23050151035@aybu.edu.tr>
2026-02-12 23:28:15 +0300

    main.py readded

---
+++ src/main.py
@@ -0,0 +1,34 @@
+import torch
+import os
+from config.config import Config
+from src.data import DataProcessor
+from src.model import GPTLanguageModel
+from src.train import Trainer
+
+def main():
+    torch.manual_seed(Config.seed)
+    
+    # Load data
+    data_processor = DataProcessor(Config.input_path)
+    
+    # Create model
+    model = GPTLanguageModel(data_processor.vocab_size).to(Config.device)
+    print(f"{sum(p.numel() for p in model.parameters())/1e6:.1f}M parameters")
+    
+    # Train
+    trainer = Trainer(model, data_processor)
+    trainer.train()
+    
+    # Generate
+    context = torch.zeros((1, 1), dtype=torch.long, device=Config.device)
+    generated = model.generate(context, max_new_tokens=2000)
+    print(data_processor.decode(generated[0].tolist()))
+    
+    # Save
+    filename = os.path.basename(Config.input_path).split('.')[0]
+    save_path = Config.model_save_path.format(filename)
+    torch.save(model.state_dict(), save_path)
+    print(f'Model saved to {save_path}')
+
+if __name__ == "__main__":
+    main()
